6b739e03ef8e8f22d2fab14637731cd602d2a1d6
Jimmy Luong <jimmyluong@yahoo.com>
2026-02-03 14:33:45 -0800

    ver 2.0 - fsm

--- FSM.asm
+++ FSM.asm
@@ -1,41 +1,48 @@
 $MODMAX10
 
-FSM1:    
+
+
+
+;key terms:
+;cjne - compare and jump if not equal
+;pwm - pulse wave modulation 
+;subb - subtract with borrow
+FSM:    
 	mov a, FSM1_state ;move the state into reg a.
 
-FSM1_state0:
-	cjne a, #0, FSM1_state1
-	mov pwm, #0
-	jb PB6, FSM1_state0_done
-	jnb PB6, $ ; Wait for key release
-	mov FSM1_state, #1
+FSM_state0:
+	cjne a, #0, FSM_state1 ;if a is not 0 move to state1
+	mov pwm, #0 ;in state 0 turn this off - power is 0%
+	jb PB6, FSM_state0_done, ;when we click the button 
+	jnb PB6, $  ; Wait for key release
+	mov FSM_state, #1
 	
-FSM1_state0_done:
-	ljmp FSM1_FSM2
+FSM_state0_done:
+	ljmp FSM2
 
-FSM1_state1:
-	cjne a, #1, FSM1_state2
-	mov pwm, #100
-	mov sec, #0
-	mov a, #150
-	clr c
-	subb a, temp
-	jnc FSM1_state1_done
-	mov FSM1_state, #2
+FSM_state1:
+	cjne a, #1, FSM_state2
+	mov pwm, #100 ;set power to 100
+	mov sec, #0 ;set the timer to 0
+	mov a, #150 ;this is the soak temperature (at 150C)
+	clr c ;clear the carry 
+	subb a, temp ;
+	jnc FSM_state1_done ;jump if no carry, 
+	mov FSM_state, #2
 	
-FSM1_state1_done:
+FSM_state1_done:
 	ljmp FSM2
 	
 	
-FSM1_state2:
-	cjne a, #2, FSM1_state3
+FSM_state2:
+	cjne a, #2, FSM_state3
 	mov pwm, #20
 	mov a, #60
 	clr c
 	subb a, sec
-	jnc FSM1_state2_done
-	mov FSM1_state, #3
+	jnc FSM_state2_done
+	mov FSM_state, #3
 	
-FSM1_state2_done:
+FSM_state2_done:
 	ljmp FSM2	
 
